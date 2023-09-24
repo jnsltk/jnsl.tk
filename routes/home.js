5005127d79b7c8a7e991eea538b7d2925e16c770
jnsltk <janos.litkei@gmail.com>
2023-09-24 16:04:20 +0200

    Finish home page

--- routes/home.js
+++ routes/home.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', function(req, res, next) {
-    res.render('home', { title: 'home' });
+    res.render('home', { title: 'jnsl.tk | home' });
 });
 
 module.exports = router;
